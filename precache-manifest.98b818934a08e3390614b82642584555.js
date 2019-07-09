101f5802181fce286851f8e0717bbfdddb5ea556
unknown <mariia>
2019-07-09 08:37:14 +0300

    Updates

---
+++ precache-manifest.98b818934a08e3390614b82642584555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435f34dbe257f82ef1ab73be26760e95",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "958a3b2e8aed0fa163d6",
+    "url": "/react_dynamic-list-of-todos/static/css/main.7a064ff3.chunk.css"
+  },
+  {
+    "revision": "469ab93ee04571d45e02",
+    "url": "/react_dynamic-list-of-todos/static/js/2.5c517716.chunk.js"
+  },
+  {
+    "revision": "958a3b2e8aed0fa163d6",
+    "url": "/react_dynamic-list-of-todos/static/js/main.9a8f4d74.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
